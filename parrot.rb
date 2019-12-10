a1e99d92bbb24f09d03b2c18e8cfe8e8eb1ede88
dwane richards <dwanerich@gmail.com>
2019-12-10 03:40:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string = "Squawk!", "Pretty bird!")
+def parrot(string = "Squawk!",var2)
   
   puts string
   "Squawk!"
