4b477f9eb7221c1400295b186106d7adc28528ab
dwane richards <dwanerich@gmail.com>
2019-12-10 03:38:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string = "Squawk!")
+def parrot(string = "Squawk!", "Pretty bird!")
   
   puts string
   "Squawk!"
 end
-  puts parrot 
-  print "Pretty bird!"
+  puts parrot("Dwane")
